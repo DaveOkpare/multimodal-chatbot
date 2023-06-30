933de7b1ad76fc8e3c1d2fd9e5fa8c496a1b3b44
DaveOkpare <okparedave@gmail.com>
2023-06-30 11:04:14 +0100

    Added telegram setup

--- main.py
+++ main.py
@@ -10,16 +10,46 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 bot = telebot.TeleBot(TELEGRAM_TOKEN, parse_mode=None)  # You can set parse_mode by default. HTML or MARKDOWN
 
 
+# Handles all text messages that contains the commands '/start' or '/help'.
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    print(message)
-    bot.reply_to(message, "Howdy, how are you doing?")
+def handle_start_help(message):
+    pass
 
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    print(message)
-    bot.reply_to(message, message.text)
+# Handles all sent documents and audio files
+@bot.message_handler(content_types=['document', 'audio'])
+def handle_docs_audio(message):
+    pass
+
+
+# Handles all text messages that match the regular expression
+@bot.message_handler(regexp="SOME_REGEXP")
+def handle_message(message):
+    pass
+
+
+# Handles all messages for which the lambda returns True
+@bot.message_handler(func=lambda message: message.document.mime_type == 'text/plain', content_types=['document'])
+def handle_text_doc(message):
+    pass
+
+
+# Which could also be defined as:
+def test_message(message):
+    return message.document.mime_type == 'text/plain'
+
+
+@bot.message_handler(func=test_message, content_types=['document'])
+def handle_text_doc(message):
+    pass
+
+
+# Handlers can be stacked to create a function which will be called if either message_handler is eligible
+# This handler will be called if the message starts with '/hello' OR is some emoji
+@bot.message_handler(commands=['hello'])
+@bot.message_handler(func=lambda msg: msg.text.encode("utf-8") == "SOME_FANCY_EMOJI")
+def send_something(message):
+    pass
 
 
 bot.infinity_polling()
